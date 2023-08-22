924abc1ed4920b913b57c03c70157595035f64a5
Juan5253 <Juan_SebastianF@hotmail.com>
2023-08-22 14:51:06 -0500

    Colors, template, navbar, footer changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
-const modal = document.querySelector('.modal')
-const button1 = document.querySelector('#button1')
+document.addEventListener("DOMContentLoaded", function () { //DOM esté completamente cargado antes de acceder a los elementos
+    const button1 = document.querySelector('#button1');
+    const closeButton = document.querySelector('.modal-content--close');
+    let modal = document.querySelector('.modal');
 
-button1.addEventListener('click', () => {
-    modal.classList.remove('hidden')
-    modal.classList.add('visible')
-})
+    button1.addEventListener('click', () => {
+        modal.classList.remove('hidden');
+        modal.classList.add('visible');
+    });
+
+    closeButton.addEventListener('click', () => {
+        modal.classList.add('hidden');
+        modal.classList.remove('visible');
+    });
+});
